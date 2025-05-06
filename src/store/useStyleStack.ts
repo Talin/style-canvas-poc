c65045648d8f34e175f7dc417147c29dabb49c4e
Talin Wadsworth <talin.wadsworth@gmail.com>
2025-05-06 14:21:30 -0600

    feat: basic StyleStack zustand store

---
+++ src/store/useStyleStack.ts
@@ -0,0 +1,13 @@
+import create from 'zustand';
+
+type Palette = { primary: string[]; neutrals: string[] };
+
+interface StyleStackState {
+  palette?: Palette;
+  setPalette: (p: Palette) => void;
+}
+
+export const useStyleStack = create<StyleStackState>((set) => ({
+  palette: undefined,
+  setPalette: (p) => set({ palette: p }),
+}));
